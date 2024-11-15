b4c95cf86b7b152bfa9f7394ffa3ada667d89fd5
william2544 <onyangowilliam2002@gmail.com>
2024-11-15 22:03:44 +0300

    my project for the assignment on odin-shpping cart

--- vite-project/src/components/Navber.jsx
+++ vite-project/src/components/Navber.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom"
 import styles from './navber.module.css'
-export default function Navber(params) {
+import {useDispatch, useSelector} from 'react-redux'
+import { useEffect } from "react"
+import { getTotalCart } from "../store/slice/cart-slice"
+
+
+export default function Navber() {
+    const cartTotalQuantity=useSelector(state=>state.cart.cartTotalQuantity)
+    const cart=useSelector(state=>state.cart.items)
+    const dispatch=useDispatch()
+
+    useEffect(()=>{
+        dispatch(getTotalCart())
+    },[cart,dispatch])
+
+
+
     return(
         <div className={styles["wrapper"]}>
             <p>Navber</p>
             <div className={styles["links"]}>
                 <Link to='/'>Home</Link>
-                <Link to='cart'>Cart</Link>
+                <div className={styles["cart-Link"]}>
+                    <Link to='cart'>Cart</Link>
+                    <span>{cartTotalQuantity}</span>
+                </div>
             </div>
         </div>
     )
--- vite-project/src/pages/cart.jsx
+++ vite-project/src/pages/cart.jsx
@@ -3,12 +3,15 @@ import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import styles from './cart.module.css'
 import { clearCart, getTotalCart } from "../store/slice/cart-slice";
+import { Link } from "react-router-dom"
 
 export default function Cart() {
     const carts = useSelector(store=>store.cart.items)
     const dispatch=useDispatch()
     const cartTotalAmount = useSelector(store => store.cart.cartTotalAmount)
     const cartTotalQuantity = useSelector(store=>store.cart.cartTotalQuantity)
+
+    const length = carts.length
     
 
     useEffect(()=>{
@@ -20,22 +23,33 @@ export default function Cart() {
     }
     return(
         <div className={styles["wrapper"]}>
-            <div className={styles["items"]}>
-                {
-                    carts.map(item=>(
-                        <CartItem key={item.id} data={item}/>
-                    ))
-                }
-            </div>
-            <div className={styles["cart-sumation"]}>
-                <div>
-                    <button onClick={handleClearCart}>Clear Cart</button>
+            {
+                length?
+                <>
+                    <div className={styles["items"]}>
+                        {
+                            carts.map(item=>(
+                                <CartItem key={item.id} data={item}/>
+                            ))
+                        }
+                    </div>
+                    <div className={styles["cart-sumation"]}>
+                        <div className={styles["clear-btn"]}>
+                            <button onClick={handleClearCart}>Clear Cart</button>
+                        </div>
+                        <div className={styles["total"]}>
+                            <p>Total Price: <span>${cartTotalAmount.toFixed(2)}</span></p>
+                            <p>Total Quantity <span>{cartTotalQuantity}</span></p>
+                        </div>
+                    </div>
+                </>
+                :<>
+                <div className={styles["cont-no-item"]}>
+                    <h3>No items yet, <Link to="/">Shop now</Link></h3>
                 </div>
-                <div className="total">
-                    <p>Total: ${cartTotalAmount.toFixed(2)}</p>
-                    <p>Total Quantity ${cartTotalQuantity}</p>
-                </div>
-            </div>
+                </>
+            }
+            
         </div>
     )    
 };
